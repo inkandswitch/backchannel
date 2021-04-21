ef6c97266e61839696d02376548a6680a18b0b30
okdistribute <633012+okdistribute@users.noreply.github.com>
2021-04-20 17:11:26 -0700

    add docs for the tests

--- test/index.js
+++ test/index.js
@@ -5,29 +5,42 @@ let { ContactId, Backchannel } = require('../src/backchannel.tsx')
 
 test('create a contact', async (t) => {
   let dbname = crypto.randomBytes(16)
+  // start a backchannel on bob and alice's devices
   let alice_device = new Backchannel(dbname + '_a')
   let bob_device = new Backchannel(dbname + '_b')
 
+  // create a document, let's say we already did the wormhole handshake
+  // TADA!!!
   let doc = crypto.randomBytes(16)
 
+  // OK, so now I create a petname for bob on alice's device..
   let petbob_id = alice_device.addContact({
     documents: [doc], moniker: 'bob'
   })
 
-  /*
-  let petalice = bob_device.addContact({
-    documents: [doc], moniker: 'alice'
-  })
-  */
-
+  // and alice can list the contacts and get bob
   let contacts = await alice_device.listContacts()
   t.equals(contacts.length, 1)
+  t.equals(contacts[0].moniker, 'bob')
+  t.equals(contacts[0].documents.length, 1)
+  t.equals(contacts[0].documents[0], doc)
+  t.ok(contacts[0].id)
 
+  // OK, now let's send bob a message 'hello'
   let outgoing = {
     contact: petbob_id,
     text: 'hello'
   }
 
+  // sending a message
+  function sendMessage ({socket, contact}) {
+    // only if the contact is bob!
+    if (contact.id === petbob_id) {
+      alice_device.sendMessage(socket, outgoing)
+    }
+  }
+
+  // what we do when bob's device has received the message
   async function onMessage ({message, documentId}) {
     console.log(message, documentId)
     t.equals(message.text, outgoing.text)
@@ -36,15 +49,13 @@ test('create a contact', async (t) => {
     await bob_device.db.delete()
   }
 
-  function sendMessage ({socket, contact}) {
-    if (contact.id === petbob_id) {
-      alice_device.sendMessage(socket, outgoing)
-    }
-  }
-
+  // bob's device has a message!
   bob_device.on('message', onMessage)
+
+  // sending the message once we an open contact
   alice_device.on('contact.open', sendMessage)
 
+  // joining the document on both sides fires the 'contact.open' event
   alice_device.joinDocument(doc)
   bob_device.joinDocument(doc)
 })
