a355e2de87f861511afb8025ff890146d1791d1f
okdistribute <633012+okdistribute@users.noreply.github.com>
2021-04-26 12:48:09 -0700

    chore: use webpack instead of browserify

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+let path = require('path')
+let DIST = path.join(__dirname, 'dist')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devServer: {
+    contentBase: DIST,
+    compress: true,
+    port: 9000,
+  },
+  output: {
+    path: DIST,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+    fallback: {
+      'crypto': require.resolve('crypto-browserify'),
+      'stream': require.resolve('stream-browserify')
+    }
+  },
+  module: {
+    rules: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      { test: /\.tsx?$/, use: ['ts-loader'], exclude: /node_modules/ },
+    ],
+  },
+};
