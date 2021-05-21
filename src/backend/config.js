78de41aa39000ad71efe6a33b574ba1b66dcac6f
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2021-05-20 18:05:53 -0700

    chore: lint

--- src/backend/config.js
+++ src/backend/config.js
@@ -1,9 +1,9 @@
-const PROD = "wss://attractive-moored-actor.glitch.me/"
-const LOCAL = "ws://localhost:3001"
+const PROD = 'wss://attractive-moored-actor.glitch.me/';
+const LOCAL = 'ws://localhost:3001';
 
 const config = {
-  "RELAY_URL": process.env.NODE_ENV === 'production' ? PROD : LOCAL
-}
-console.log(config.RELAY_URL)
+  RELAY_URL: process.env.NODE_ENV === 'production' ? PROD : LOCAL,
+};
+console.log(config.RELAY_URL);
 
 export default config;
