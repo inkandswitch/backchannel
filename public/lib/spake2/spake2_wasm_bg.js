63361351b8c134751c49e461d1aff94029858805
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2021-08-05 22:24:16 -0700

    Start packaging into a library

--- public/lib/spake2/spake2_wasm_bg.js
+++
@@ -1,9 +0,0 @@
-
-const path = require('path').join(__dirname, 'spake2_wasm_bg.wasm');
-const bytes = require('fs').readFileSync(path);
-let imports = {};
-imports['./spake2_wasm.js'] = require('./spake2_wasm.js');
-
-const wasmModule = new WebAssembly.Module(bytes);
-const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
-module.exports = wasmInstance.exports;
